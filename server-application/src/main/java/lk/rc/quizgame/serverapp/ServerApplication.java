e32d89e688b2398c033270ec75bd3801d0e88af6
rchathuranga <ravindu_j@epiclanka.net>
2021-11-20 22:14:41 +0530

    Initiate Server Application

---
+++ server-application/src/main/java/lk/rc/quizgame/serverapp/ServerApplication.java
@@ -0,0 +1,13 @@
+package lk.rc.quizgame.serverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
